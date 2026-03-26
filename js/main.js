254f9ffcecf85abb21c18ee835402b50a6a390e0
JJonLumibao <jjonlumibao@gmail.com>
2026-03-26 18:25:51 -0400

    Added js animations and redesigned project tab

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const root = document.documentElement;
+const inView = 'in-view';
 
 const openMenu = document.querySelector('.menu-btn');
 const closeMenus = document.querySelectorAll('[data-close]');
 const menu = document.querySelector('#menu');
+const scrollAnimate = document.querySelectorAll('.scroll-animate');
 
 openMenu.addEventListener('click', function() {
   menu.classList.toggle('open');
@@ -13,3 +15,23 @@ closeMenus.forEach(btn => {
     menu.classList.remove('open');
   });
 });
+
+function respondToVisibility(elm, cb) {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        cb(true);
+        observer.unobserve(elm);
+      }
+    });
+  }, {threshold: 0.2});
+  observer.observe(elm);
+}
+
+for(const elm of scrollAnimate) {
+  respondToVisibility(elm, (isVisible) => {
+    if(isVisible) {
+      elm.classList.add(inView);
+    }
+  });
+}
